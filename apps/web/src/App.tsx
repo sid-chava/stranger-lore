892a0a9854327bb75151a614310f25c3094b0862
Sid Chava <sc9423@nyu.edu>
2025-11-18 12:40:43 -0500

    Remove Cloudflare outage banner

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -12,7 +12,6 @@ import TopTheoriesPage from './pages/TopTheoriesPage';
 import ContributorLeaderboardPage from './pages/ContributorLeaderboardPage';
 import UsernamePrompt from './components/UsernamePrompt';
 import { AuthModalProvider } from './components/AuthModal';
-import { CloudflareBanner } from './components/CloudflareBanner';
 
 function HandlerRoutes() {
   const location = useLocation();
@@ -68,7 +67,6 @@ function App() {
       <StackProvider app={stackClientApp}>
         <StackTheme>
           <AuthModalProvider>
-            <CloudflareBanner />
             <UsernamePrompt />
             <AppRoutes />
           </AuthModalProvider>
